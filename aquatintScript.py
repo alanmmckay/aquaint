ac5c697dea774efd8dde1ea3360887a3bff5d1c2
alanmmckay <alanmmckay@gmail.com>
2022-05-31 20:43:37 -0500

    Enabled a means to safe to file multiple images which show the progress of the process. Should consider some new arguments to running the python script which sets whether or not these writes occur.

--- aquatintScript.py
+++ aquatintScript.py
@@ -11,20 +11,8 @@ import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import scipy.misc
-#import imageio
-
-#---
-# Added these guys:
-#from IPython import get_ipython
-#from IPython.terminal.interactiveshell import TerminalInteractiveShell
-#shell = TerminalInteractiveShell.instance()
 
 import imageio.v2 as imageio
-
-#moved this :
-#get_ipython().run_line_magic('matplotlib', 'inline')
-#---
-
 
 im2 = imageio.imread(filename)
 Nix=im2.shape[0]
@@ -39,9 +27,9 @@ for i in range(0,Nix):
             grayimage[i][j] = grayValue
             pass
 dsqin=1-grayimage/255.0
-#hsimage=plt.imshow(dsqin,cmap='Greys',aspect=1,interpolation='none')
-#plt.colorbar(hsimage)
-#plt.show(hsimage)
+hsimage=plt.imshow(dsqin,cmap='Greys',aspect=1,interpolation='none')
+cb = plt.colorbar(hsimage)
+plt.savefig(filename.split('.')[-2]+'-origin.jpg',dpi=300)
 
 #################################
 
@@ -54,9 +42,8 @@ for jj in range(nsites):
             pass
 
 dsq=np.reshape(hhbw,(Nix,Niy))
-#hsimage=plt.imshow(dsq,cmap='Greys',aspect=1,interpolation='none')
-#plt.colorbar(hsimage)
-#plt.show(hsimage)
+hsimage=plt.imshow(dsq,cmap='Greys',aspect=1,interpolation='none')
+plt.savefig(filename.split('.')[-2]+'-greycut.jpg',dpi=300)
 
 sth=1
 ########################
@@ -85,18 +72,15 @@ for nsweeps in range(totalsweeps):
         pass
     v=((sig+1)/2)
     dsq=np.reshape(v,(Ny,Nx))
-    #hsimage=plt.imshow(dsq,cmap='Greys',aspect=1,interpolation='none')
-    #plt.axis('off')
-    #plt.show(hsimage)
+    hsimage=plt.imshow(dsq,cmap='Greys',aspect=1,interpolation='none')
+    plt.savefig(filename.split('.')[-2]+'-sweep'+str(nsweeps)+'.jpg',dpi=300)
     pass
 
 hsimage=plt.imshow(dsq,cmap='Greys',aspect=1,interpolation='none')
+cb.remove()
 plt.axis('off')
-#plt.savefig("mypict4.jpg",dpi=300)
 plt.savefig(filename.split('.')[-2]+'-aquatint.jpg',dpi=300)
-#plt.show(hsimage)
 plt.close()
-
 
 
 
