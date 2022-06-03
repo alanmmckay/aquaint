307eb1873d716a5acc22bed81eee709564585538
alanmmckay <alanmmckay@gmail.com>
2022-06-03 11:05:52 -0500

    Removed bootstrap folder for the sake of github analytics. Using external links to import the bootstrap information.

--- submit.php
+++ submit.php
@@ -2,8 +2,8 @@
 <html>
     <head>
         <meta name='viewport' content='width=device-width, initial-scale=1'>
-        <link rel='stylesheet' href='bootstrap/css/bootstrap.min.css'>
-        <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
         <script src='bootstrap/js/bootstrap.min.js'></script>
         <!--style>
             main{
